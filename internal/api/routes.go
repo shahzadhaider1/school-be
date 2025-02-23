d5eaccaf854c60c97c15e1b5bc85d5ab27ccd09c
Shahzad Haider <shahzadhaider.se@gmail.com>
2025-02-23 23:44:41 +0500

    added class handlers

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -1,9 +1,20 @@
 package api
 
 import (
+	"school-be/internal/service"
+
 	"github.com/gofiber/fiber/v3"
 )
 
 func SetUpRoutes(app *fiber.App) {
+	api := app.Group("/api")
 
+	classService := service.NewClassService()
+	classHandler := NewClassHandler(classService)
+
+	api.Post("/classes", classHandler.CreateClassHandler)
+	api.Get("/classes/:id", classHandler.GetClassByIDHandler)
+	api.Get("/classes", classHandler.ListClassesHandler)
+	api.Put("/classes/:id", classHandler.UpdateClassHandler)
+	api.Delete("/classes/:id", classHandler.DeleteClassHandler)
 }
